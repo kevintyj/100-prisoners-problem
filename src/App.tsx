c2b36d93c503be76341a16d6f627a191cb713595
kevintyj <hello@kevintyj.com>
2022-07-06 18:46:47 -0400

    rollback(start): Rollback to init

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,32 @@ import type { Component } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { simulate } from './functions/problem';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+      <div class={styles.App}>
+        <header class={styles.header}>
+          <img src={logo} class={styles.logo} alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            class={styles.link}
+            href="https://github.com/solidjs/solid"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Solid
+          </a>
+        </header>
+      </div>
+
+      <h1>
+        Result {simulate(100, 1)}
+      </h1>
+    </>
   );
 };
 
